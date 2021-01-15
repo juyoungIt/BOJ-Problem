eb51e4b342d2c9e374452bc9c7f5887d3f4be463
JuyoungHa <21700760@handong.edu>
2021-01-15 09:54:49 +0900

    Q.2941 Solution Update(initial version)

--- 2941.c
+++ 2941.c
@@ -8,44 +8,44 @@
 
 #define TRUE 1
 #define FALSE 0
-#define MAXSIZE 100
 
 // 함수 선언부
-int validCheck(char* str, int len); // 입력 문자열에 대한 유효성 검사
-int specialChar(char c); // 특수한 문자를 구성하는 문자인지 여부를 판별
-int alCount(char* str, int len); // 입력 문자열을 구성하는 크로아티아 알파벳의 수를 카운트
+int validCheck(char* str, int len); // 입력 문자열의 유효성 검사
+int alCount(char* str, int len);    // 문자열에 존재하는 크로아티아 문자수 카운트
+int isStart(char c); // 입력문자가 특수문자를 시작하는 문자인지 판별
+int isEnd(char c); // 입력문자가 특수문자를 마무리하는 문자인지 판별
 
 int main()
 {
-    char str[MAXSIZE+1]; // 입력 문자열
+    // 0. 필요한 변수선언
+    char str[100+1]; // 입력 문자열
+    int result; // 크로아티아 문자 수
     int len; // 입력 문자열의 길이
-    int count; // 크로아티아 알파벳의 수
 
-    // 1. 문자열 입력 + 유효성 검사
+    // 1. 문자열 입력 (+ 유효성 검사)
     scanf("%s", str);
     len = (int)strlen(str);
     if(validCheck(str, len) == FALSE) {
-        printf("Error! - invalid input string.\n");
+        printf("Error! - invalid string input.\n");
         return -1;
     }
 
-    // 2. 크로아티아 알파벳의 수를 카운트함
-    count = alCount(str, len);
+    // 2. 크로아티아 문자의 수 카운트
+    result = alCount(str, len);
 
     // 3. 카운트 결과를 출력
-    printf("%d\n", count);
+    printf("%d\n", result);
 
-    // 4. 프로그램을 종료
+    // 4. 프로그램 종료
     return 0;
 }
 
 // 함수 정의부
 int validCheck(char* str, int len)
 {
-    char tmp; // 문자 1개를 임시저장
-
+    char tmp; // 현재 조회 중인 문자
     for(int i=0 ; i<len ; i++) {
-        tmp = str[i]; // 문자 1개를 읽어옴
+        tmp = str[i]; // 문자 1개 읽기
         if(!((tmp>=97 && tmp<=122) || tmp == '-' || tmp == '='))
             return FALSE;
     }
@@ -54,42 +54,85 @@ int validCheck(char* str, int len)
 
 int alCount(char* str, int len)
 {
-    int result = 0; // 카운트된 크로아티아 알파벳의 수
-    int special = FALSE; // 특수문자를 읽는 중인지에 대한 여부
-    char biprev = -1; // 이전이전 문자를 임시저장
-    char prev = -1; // 이전 문자를 임시저장
-    char cur; // 문자 1개를 임시저장
+    char cur; // 현재 조회하는 문자
+    char prev; // 이전에 조회한 문자
+    char special = FALSE; // 특수문자 리딩상태 여부
+    int count = 0; // 문자열에 존재하는 알파벳 카운트의 수
 
     for(int i=0 ; i<len ; i++) {
-        cur = str[i]; // 1개의 문자를 가져옴
-        // 특수한 경우를 제외한 일반적인 1자의 알파벳인 경우
-        if(specialChar(cur) == FALSE) {
-            if(special == FALSE)
-                result++;
+        cur = str[i]; // 문자 1개 로딩
+        // 특수문자를 읽고 있는 상황인 경우
+        if(special == TRUE) {
+            // 특수문자를 마무리하는 문자인 경우
+            if(isEnd(cur)) {
+                // 현재 문자가 '-'인 경우
+                if(cur == '-') {
+                    // 이전문자가 'c' 또는 'd'
+                    if(prev == 'c' || prev == 'd') {
+                        count++; // 알파벳 수 1증가
+                        special = FALSE; // 특수문자 리딩상태 업데이트
+                    }
+                }
+                // 현재 문자가 '='인 경우
+                else if(cur == '=') {
+                    // 이전 문자가 'c' 또는 'z' 또는 's'인 경우
+                    if(prev == 'c' || prev == 'z' || prev == 's') {
+                        count++; // 알파벳 수 1증가
+                        special = FALSE; // 특수문자 리딩상태 업데이트
+                    }
+                }
+                // 현재 문자가 'j'인 경우
+                else if(cur == 'j') {
+                    // 이전 문자가 'l' 또는 'n'인 경우
+                    if(prev == 'l' || prev == 'n') {
+                        count++; // 알파벳 수 1증가
+                        special = FALSE; // 특수문자 리딩상태 업데이트
+                    }
+                }
+            }
+            // 특수문자를 마무리하는 문자가 아닌 경우
             else {
-                special = FALSE;
-                if(biprev == 'b' && prev == 'z') result += 3;
-                else result += 2;
+                // 특수문자를 시작하는 문자인 경우
+                if(isStart(cur)) {
+                    // 이전문자가 d, 현재문자가 z가 아닌 경우
+                    if(!(prev == 'd' && cur == 'z')) {
+                        count++; // 알파벳 수 1증가
+                    }
+                }
+                // 특수문자를 시작하는 문자가 아닌 경우
+                else {
+                    count += 2; // 알파벳 수 2증가
+                    special = FALSE; // 특수문자 리딩 상태 업데이트
+                }
             }
         }
-        // 특수한 경우인 2자 또는 3자 알파벳을 구성하는 알파벳인 경우
+        // 특수문자를 리딩하는 상황이 아닌 경우
         else {
-            special = TRUE;
-            if((prev == 'c' && cur == '=') || (prev == 'c' && cur == '-')) result++;
-            else if(prev == 'd' && cur == '-') result++;
-            else if((prev == 'l' && cur == 'j') || (prev == 'n' && cur == 'j')) result++;
-            else if((prev == 's' && cur == '=') || (prev == 'z' && cur == '=')) result++;
-            else if(biprev == 'b' && prev == 'z' && cur == '=') result++;
+            // 특수문자를 시작하는 문자인 경우
+            if(isStart(cur)) {
+                special = TRUE; // 특수문자 리딩상태 업데이트
+            }
+            // 특수문자를 시작하는 문자가 아닌 경우
+            else {
+                count++; // 알파벳의 수 1증가
+            }
         }
-        biprev = prev; // 이전 문자를 백업
-        prev = cur;    // 현재 문자를 백업
+        prev = cur; // 현재 조회 중인 문자를 백업
     }
-    return result;
+    return count; // 산출한 카운트 결과 반환
 }
 
-int specialChar(char c)
+int isStart(char c)
 {
-    if(c == 'c' || c == 'd' || c == 'l' || c == 'n' || c == 's' || c == 'z' || c == '-' || c == '=' || c == 'j')
+    if(c == 'c' || c == 'd' || c == 'l' || c == 'n' || c == 's' || c == 'z')
+        return TRUE;
+    else
+        return FALSE;
+}
+
+int isEnd(char c)
+{
+    if(c == '-' || c == '=' || c == 'j')
         return TRUE;
     else
         return FALSE;
