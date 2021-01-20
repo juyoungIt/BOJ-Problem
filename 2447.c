574ff517fb5100096bca1e5b9cde5a5c1e34d319
JuyoungHa <21700760@handong.edu>
2021-01-20 20:36:40 +0900

    Q.2447 baseline code

---
+++ 2447.c
@@ -0,0 +1,25 @@
+/*
+ * Q.2447 Solution Source Code
+ * Writer : JuyoungIt
+ */
+
+#include <stdio.h>
+
+// declaration of function
+void drawStar(int n); // draw 'N' pattern star
+
+int main()
+{
+    int size; // pattern size
+
+    scanf("%d", &size); // input pattern size
+    drawStar(size);     // draw 'N' pattern star
+    return 0;
+}
+
+// definition of function
+void drawStar(int n)
+{
+    // base case
+    // general case
+}
