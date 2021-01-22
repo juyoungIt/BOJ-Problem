fdb5f1adbf2c81d913db37adfdaa05789b6ac02f
JuyoungHa <21700760@handong.edu>
2021-01-22 15:47:00 +0900

    Q.11729 Solution Update

--- 11729.c
+++ 11729.c
@@ -10,26 +10,41 @@
 #define NUM3 3 // bar3 - destination
 
 // declaration of function
-void hanoi(int n, int src, int stp, int dst);
+void countHanoi(int* mc, int n); // count number of moves
+void printHanoi(int n, int src, int stp, int dst); // print each moves
 
 int main()
 {
     int blockCount;     // number of blocks
+    int** solution;     // save hanoi tower solution
     int moveCount = 0;  // number of moves
 
     scanf("%d", &blockCount);
-    hanoi(blockCount, NUM1, NUM2, NUM3);
+    countHanoi(&moveCount, blockCount);
+    printf("%d\n", moveCount);
+    printHanoi(blockCount, NUM1, NUM2, NUM3);
     return 0;
 }
 
 // definition of function
-void hanoi(int n, int src, int stp, int dst)
+void countHanoi(int* mc, int n)
+{
+    if(n == 1)
+        (*mc)++;
+    else {
+        countHanoi(mc, n-1);
+        (*mc)++;
+        countHanoi(mc, n-1);
+    }
+}
+
+void printHanoi(int n, int src, int stp, int dst)
 {
     if(n == 1)
         printf("%d %d\n", src, dst);
     else {
-        hanoi(n-1, src, dst, stp);
+        printHanoi(n-1, src, dst, stp);
         printf("%d %d\n", src, dst);
-        hanoi(n-1, stp, src, dst);
+        printHanoi(n-1, stp, src, dst);
     }
 }
