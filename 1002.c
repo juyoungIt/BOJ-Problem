1262f19d971f2c26ffb8de20ca2fd4e93da65338
JuyoungHa <21700760@handong.edu>
2021-02-10 23:10:45 +0900

    Q.1002 Solution Update

--- 1002.c
+++ 1002.c
@@ -7,60 +7,60 @@
 #include <stdlib.h>
 #include <math.h>
 
-int calDist(int* tc);
-int eSearch(int* tc);
+double calDist(int* tc);
+int eSearch(double dist, int r1, int r2);
 
 int main()
 {
-    int tcNum;
-    int** testCase;
+    int tcNum; // the number of test case
+    int** tc;  // store all test case
 
     scanf("%d", &tcNum);
-    testCase = (int**)malloc(sizeof(int*) * tcNum);
+    tc = (int**)malloc(sizeof(int*) * tcNum);
     for(int i=0 ; i<tcNum ; i++) {
-        testCase[i] = (int*)malloc(sizeof(int) * 6);
-        scanf("%d %d %d %d %d %d", &testCase[i][0], &testCase[i][1], &testCase[i][2],
-              &testCase[i][3], &testCase[i][4], &testCase[i][5]);
+        tc[i] = (int*)malloc(sizeof(int) * 6);
+        scanf("%d %d %d %d %d %d", &tc[i][0], &tc[i][1], &tc[i][2], &tc[i][3], &tc[i][4], &tc[i][5]);
     }
 
     for(int i=0 ; i<tcNum ; i++)
-        printf("%d\n", eSearch(testCase[i]));
+        printf("%d\n", eSearch(calDist(tc[i]), tc[i][2], tc[i][5]));
 
     for(int i=0 ; i<tcNum ; i++)
-        free(testCase[i]);
-    free(testCase);
+        free(tc[i]);
+    free(tc);
     return 0;
 }
 
-int calDist(int* tc)
+double calDist(int* tc)
 {
-    int distX, distY;
-    int dist;
-
-    distX = (int)abs(tc[0] - tc[3]); // |x1-x2|
-    distY = (int)abs(tc[1] - tc[4]); // |y1-y2|
-    dist = (int)pow(distX, 2) + (int)pow(distY, 2);
+    double dist;
+    dist = sqrt(pow(abs(tc[0]-tc[3]), 2) + pow(abs(tc[1]-tc[4]), 2));
     return dist;
 }
 
-int eSearch(int* tc)
+int eSearch(double dist, int r1, int r2)
 {
-    int dist = calDist(tc);
-    int len; // the value of r1+r2
+    int bigR, smallR;
 
-    len = tc[2] + tc[5]; // r1+r2
-    if(dist == 0) {
-        if(tc[2] == tc[5]) // r1 == r2
-            return -1;
-        else // r1 != r2
-            return 0;
-    }
+    bigR = (r1>r2) ? r1 : r2;
+    smallR = (r1>r2) ? r2 : r1;
+    if(dist > r1+r2)
+        return 0;
+    else if(dist == r1+r2)
+        return 1;
     else {
-        if(dist == (int)pow(len,2))
+        if(dist!=0 && dist+smallR == bigR)
             return 1;
-        else if(dist < pow(len, 2))
-            return 2;
-        else
+        else if(dist+smallR < bigR) {
+            if(dist==0 && r1!=r2)
+                return 0;
             return 0;
+        }
+        else {
+            if(dist==0 && r1==r2)
+                return -1;
+            else
+                return 2;
+        }
     }
 }
