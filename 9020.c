5829b4603715921a242feee2f9578bfe04ce3643
JuyoungHa <21700760@handong.edu>
2021-02-15 20:55:24 +0900

    Q.9020 Update(ing...)

--- 9020.c
+++ 9020.c
@@ -10,33 +10,29 @@ int isPrime(int* list, int n, int size);
 
 int main()
 {
-    int tcSize; // the number of test case
-    int* tc;    // store test case
-    int p1, p2; // partition1 & 2
-    int* pn;    // the list of prime number
-    int* pList; // the list of prime number(except zero)
-    int pSize = 0;  // the number of prime number
-    int size;
-    int num;
+    int tcSize;      // the number of test case
+    int* tc;         // store test case
+    int p1, p2;      // partition1 & 2
+    int* pn;         // the list of prime number
+    int* pList=NULL; // the list of prime number(except zero)
+    int pSize = 0;   // the number of prime number
+    int size;        // the size of search
 
     scanf("%d", &tcSize);
     tc = (int*)malloc(sizeof(int) * tcSize);
     for(int i=0 ; i<tcSize ; i++)
         scanf("%d", &tc[i]);
 
-    // access each test case
     for(int i=0 ; i<tcSize ; i++) {
-        num = tc[i];
         pn = (int*)malloc(sizeof(int) * (tc[i] + 1));
-        for(int j=2 ; j<=num ; j++)
+        for(int j=2 ; j<=tc[i] ; j++)
             pn[j] = j;
-        for(int j=2 ; j<=num ; j++) {
+        for(int j=2 ; j<=tc[i] ; j++) {
             if(pn[j] == 0) continue;
-            for(int k=j+j ; k<=num ; k+=j)
+            for(int k=j+j ; k<=tc[i] ; k+=j)
                 pn[k] = 0;
         }
-        // calculate the number of prime number
-        for(int j=2 ; j<=num ; j++) {
+        for(int j=2 ; j<=tc[i] ; j++) {
             if(pn[j] != 0) {
                 pList = (int*)realloc(pList, sizeof(int) * (pSize+1));
                 pList[pSize] = pn[j];
@@ -44,10 +40,10 @@ int main()
             }
         }
         size = (pSize%2 == 0) ? pSize/2 : pSize/2+1; // calculate search size
-        for(int j=2 ; j<=size+2 ; j++) {
-            if(isPrime(pList, num-pList[j], pSize) == 1) {
+        for(int j=0 ; j<size ; j++) {
+            if(isPrime(pList, tc[i]-pList[j], pSize) == 1) {
                 p1 = pList[j];
-                p2 = num-p1;
+                p2 = tc[i]-p1;
             }
         }
         printf("%d %d\n", p1, p2);
@@ -60,7 +56,7 @@ int main()
 
 int isPrime(int* pList, int num, int pSize)
 {
-    for(int i=2 ; i<pSize ; i++) {
+    for(int i=0 ; i<pSize ; i++) {
         if(pList[i] == num) return 1;
     }
     return 0;
